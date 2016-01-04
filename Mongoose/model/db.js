35730c1a69f3c98e23cf63cc3c532143f79cd1ba
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2016-01-04 17:54:19 +0100

    Some minor improvements to connect to MongoDB.

--- Mongoose/model/db.js
+++ Mongoose/model/db.js
@@ -3,8 +3,15 @@
  */
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/catalog", function () {
+mongoose.connect("mongodb://localhost/catalog");
+var db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error."));
+db.on("open", function () {
     console.log("Mongodb connected");
 });
+
+//mongoose.connect("mongodb://localhost/catalog", function () {
+//    console.log("Mongodb connected");
+//});
 
 module.exports = mongoose;
