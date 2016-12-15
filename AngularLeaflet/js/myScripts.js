cc515b0527ac6376656f054ea21f8700da34480e
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2016-12-15 17:23:50 +0100

    Ahora funciona sin necesidad de servidor, faltaba tiles.

--- AngularLeaflet/js/myScripts.js
+++ AngularLeaflet/js/myScripts.js
@@ -5,10 +5,16 @@ var app = angular.module("app", ["leaflet-directive"]);
 
 app.controller("TheController", ["$scope", function($scope) {
     angular.extend($scope, {
-        castellon: {
-            lat: 39.98685368305097,
-            lng: -0.04566192626953125,
-            zoom: 14
-        }
+      castellon: {
+          lat: 39.98685368305097,
+          lng: -0.04566192626953125,
+          zoom: 14
+      },
+      tiles: {
+        url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+           options: {
+               attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+             }
+      }
     })
 }]);
